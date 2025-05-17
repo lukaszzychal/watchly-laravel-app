1f0761bc656d2d7241db551db390212f59fd2723
Łukasz Zychal <zychallukasz@gmail.com>
2025-05-17 15:37:07 +0200

    change pest to phpunit

--- tests/Feature/MyTest.php
+++ tests/Feature/MyTest.php
@@ -8,70 +8,70 @@ use Tests\TestCase;
 
 class MyTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function the_application_returns_a_successful_response_in_the_home_page(): void
-    {
-        $this->get('/')
-            ->assertStatus(200)
-            ->assertOk()
-            ->assertSee([
-                '<title>Watchly - </title>',
-                ''
-            ], false);
-    }
-
-    /**
-     * @test
-     */
-    public function the_application_returns_a_successful_response_in_the_movies_page(): void
-    {
-         $this->get('/movies')
-            ->assertStatus(200)
-            ->assertOk()
-            ->assertSee([
-                '<title>Watchly - Movies</title>',
-            ], false);
-    }
-
-    /**
-     * @test
-     */
-    public function the_application_returns_a_successful_response_in_the_series_page(): void
-    {
-        $this->get('/series')
-            ->assertStatus(200)
-            ->assertOk()
-            ->assertSee([
-                '<title>Watchly - Series</title>'
-            ], false);
-    }
-
-    /**
-     * @test
-     */
-    public function the_application_returns_a_successful_response_in_the_cinema_page(): void
-    {
-        $this->get('/cinema')
-            ->assertStatus(200)
-            ->assertOk()
-            ->assertSee([
-                '<title>Watchly - Cinema</title>'
-            ], false);
-    }
-
-    /**
-     * @test
-     */
-    public function the_application_returns_a_successful_response_in_the_tv_page(): void
-    {
-        $this->get('/tv')
-            ->assertStatus(200)
-            ->assertOk()
-            ->assertSee([
-                '<title>Watchly - TV</title>'
-            ], false);
-    }
+//    /**
+//     * @test
+//     */
+//    public function the_application_returns_a_successful_response_in_the_home_page(): void
+//    {
+//        $this->get('/')
+//            ->assertStatus(200)
+//            ->assertOk()
+//            ->assertSee([
+//                '<title>Watchly - </title>',
+//                ''
+//            ], false);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function the_application_returns_a_successful_response_in_the_movies_page(): void
+//    {
+//         $this->get('/movies')
+//            ->assertStatus(200)
+//            ->assertOk()
+//            ->assertSee([
+//                '<title>Watchly - Movies</title>',
+//            ], false);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function the_application_returns_a_successful_response_in_the_series_page(): void
+//    {
+//        $this->get('/series')
+//            ->assertStatus(200)
+//            ->assertOk()
+//            ->assertSee([
+//                '<title>Watchly - Series</title>'
+//            ], false);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function the_application_returns_a_successful_response_in_the_cinema_page(): void
+//    {
+//        $this->get('/cinema')
+//            ->assertStatus(200)
+//            ->assertOk()
+//            ->assertSee([
+//                '<title>Watchly - Cinema</title>'
+//            ], false);
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function the_application_returns_a_successful_response_in_the_tv_page(): void
+//    {
+//        $this->get('/tv')
+//            ->assertStatus(200)
+//            ->assertOk()
+//            ->assertSee([
+//                '<title>Watchly - TV</title>'
+//            ], false);
+//    }
 
 }

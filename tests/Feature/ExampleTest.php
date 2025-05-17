f210e3b1119ce3caa79710357f7196902369ed9a
Łukasz Zychal <zychallukasz@gmail.com>
2025-05-17 14:11:42 +0200

    test pest test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,25 +2,32 @@
 
 use function Pest\Laravel\get;
 
-test('the application returns a successful response in the home page', function () {
+
+test('the application returns a successful response in the test', function () {
     $this->withoutExceptionHandling();
-   $response = get('/');
+   $response = get('/test');
     $response->assertStatus(200);
 });
 
-test('the application returns a successful response in the movies page', function () {
-    $this->withoutExceptionHandling();
-    get('/movies')->assertStatus(200);
-});
-
-test('the application returns a successful response in the series page', function () {
-    get('/series')->assertStatus(200);
-});
-
-test('the application returns a successful response in the cinema page', function () {
-    get('/cinema')->assertStatus(200);
-});
-
-test('the application returns a successful response in the tv page', function () {
-    get('/tv')->assertStatus(200);
-});
+//test('the application returns a successful response in the home page', function () {
+//    $this->withoutExceptionHandling();
+//   $response = get('/');
+//    $response->assertStatus(200);
+//});
+//
+//test('the application returns a successful response in the movies page', function () {
+//    $this->withoutExceptionHandling();
+//    get('/movies')->assertStatus(200);
+//});
+//
+//test('the application returns a successful response in the series page', function () {
+//    get('/series')->assertStatus(200);
+//});
+//
+//test('the application returns a successful response in the cinema page', function () {
+//    get('/cinema')->assertStatus(200);
+//});
+//
+//test('the application returns a successful response in the tv page', function () {
+//    get('/tv')->assertStatus(200);
+//});

0a74bf0e88fb0b3b79b1005147053981edaba711
Łukasz Zychal <zychallukasz@gmail.com>
2025-05-17 15:12:01 +0200

    fix pest test

---
+++ tests/Feature/MyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testMy(): void
+    {
+        $this->assertTrue(true);
+   }
+}
